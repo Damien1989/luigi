6b5e54adf937a452922977c4563308d6113cbb9c
damien <contact.damiendiaz@gmail.com>
2023-02-21 20:56:20 +0100

    quest 9

---
+++ src/DataFixtures/ProgramFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Program;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class ProgramFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+       $program = new Program();
+       $program->setTitle("Matrix");
+       $program->setSynopsis("Neo is a bad guy !");
+       $program->setPoster("Hello");
+       $program->setCategory($this->getReference('category_Action'));
+       $manager->persist($program);
+
+
+        $manager->flush();
+    }
+
+    public function getDependencies()
+    {
+        return [
+            CategoryFixtures::class
+        ];
+    }
+}
